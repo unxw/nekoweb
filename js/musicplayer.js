542144cfebabe515ce20ace7476d5c8550462aa8
nixora maybe <nixora@nixora.space>
2026-08-14 10:30:16 -0400

    musicplayer lots of work done

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -1,22 +1,48 @@
-var defaultsong = "/assets/audio/newlook.mp3"; // i will impliment this later when ia add some sort of settings menu
-var song = new Audio(defaultsong);
+var song = null;
+var songs = [];
+var songNum = 0;
+
+function initSongs(){
+    const songList = "/assets/audio/songs.json";
+    fetch(songList)
+        .then(response => response.json())
+        .then(data => {songs = data.songs;
+            song = new Audio(songs[songNum].url || []);
+            song.addEventListener("ended", (event) => {
+                if(songNum > songs.length) {
+                    songNum = 0;
+                }
+                songNum++;
+                song.src = songs[songNum].url;
+                playSong();
+            });
+    });
+    console.log ("songs loaded ok!!");
+}
 
 function playSong() {
-     if (song.readyState > HTMLMediaElement.HAVE_CURRENT_DATA) {  // palys audio when it is value 3 or higher, meaning it can play from beginning to end uninterrupted
-        song.play();
-        return("song playing");
-     }
-     else{
-        return ("song isnt loading correctly... please check the file or try again");
-     }
+    if(!song) return ("no song loaded yet");
+    if(song.readyState < 3) song.load();
+    song.play();
+    getSongInfo();
+
 }
 
 function pauseSong() {
+    if(!song) return ("no song loaded to pause");
     song.pause();
     return ("song paused");
 }
 
 function setVolume(vol){
+    if(!song) return ("no song loaded to set volume");
     song.volume = vol;
     return ("volume set to " + vol*100 + "% :P");
+}
+
+function getSongInfo() {
+    if(!song) return ("no song loaded to get info");
+    console.log(songs);
+    console.log("Song: " + songs[songNum].title +
+                "\nArtist: " + songs[songNum].artist);
 }
